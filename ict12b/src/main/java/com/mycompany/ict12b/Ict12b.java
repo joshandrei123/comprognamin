6951968c4884d785924cafcdc4907ea2da14a807
Gamer <Gamer@DESKTOP-GG1GBHG>
2025-04-11 21:23:41 +0800

    josh

---
+++ ict12b/src/main/java/com/mycompany/ict12b/Ict12b.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ict12b;
+
+/**
+ *
+ * @author Gamer
+ */
+public class Ict12b {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
